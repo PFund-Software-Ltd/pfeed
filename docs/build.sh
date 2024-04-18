bac66e5f0a8a87bfd2bf46d8bfc8142f0d999bc6
stephenyau <softwareentrepreneer@gmail.com>
2024-04-18 16:32:15 +0800

    fix: doc buliding

--- docs/build.sh
+++ docs/build.sh
@@ -2,8 +2,14 @@
 
 echo "Google Analytics ID: $GA_TRACKING_ID"
 
+echo "Before replacement:"
+grep "google_analytics_id" _config.yml
+
 # Replace the placeholder in the YAML file with the actual Google Analytics ID
 sed -i "s/{GA_TRACKING_ID_PLACEHOLDER}/$GA_TRACKING_ID/" _config.yml
+
+echo "After replacement:"
+grep "google_analytics_id" _config.yml
 
 # Build
 jupyter-book build docs/ --all

aaf4ca07f9c561264eb9229801ee1b57b95a78f5
stephenyau <softwareentrepreneer@gmail.com>
2024-04-18 16:25:54 +0800

    docs: update theme

--- docs/build.sh
+++ docs/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Google Analytics ID: $GA_TRACKING_ID"
+
 # Replace the placeholder in the YAML file with the actual Google Analytics ID
 sed -i "s/{GA_TRACKING_ID_PLACEHOLDER}/$GA_TRACKING_ID/" _config.yml
 

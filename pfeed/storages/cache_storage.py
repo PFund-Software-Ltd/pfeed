3d6824b6b1aeb9ed7d38d99800076932ca70c49d
softwareentrepreneer <softwareentrepreneer@gmail.com>
2025-01-02 16:55:00 +0800

    fix: remove per-date cache storage

--- pfeed/storages/cache_storage.py
+++ pfeed/storages/cache_storage.py
@@ -16,23 +16,23 @@ class CacheStorage(LocalStorage):
     def _create_data_path(self) -> Path:
         from pfeed.config_handler import get_config
         config = get_config()
-        today = self._get_today()
-        # group the storages by date, easier to be cleaned up
-        return Path(config.cache_path) / str(today)
+        return Path(config.cache_path)
 
     def clear_caches(self):
         '''Clear old caches except the current date'''
         from pfund import print_error
+        from pfund.utils.utils import get_last_modified_time
         from pfeed.config_handler import get_config
         config = get_config()
         cache_path = config.cache_path
         today = self._get_today()
-        for date_dir in os.listdir(cache_path):
-            full_path = os.path.join(cache_path, date_dir)
-            date = datetime.datetime.strptime(date_dir, '%Y-%m-%d').date()
+        
+        for file_dir in os.listdir(cache_path):
+            full_path = os.path.join(cache_path, file_dir)
+            last_modified_date = get_last_modified_time(full_path).date()
             try:
-                if os.path.isdir(full_path) and date < today - datetime.timedelta(days=self.NUM_RETAINED_DAYS):
+                if os.path.isdir(full_path) and last_modified_date < today - datetime.timedelta(days=self.NUM_RETAINED_DAYS):
                     shutil.rmtree(full_path)
-                    print(f"Removed '{date_dir}' directory in caches")
+                    print(f"Removed '{file_dir}' directory in caches")
             except Exception as e:
-                print_error(f"Error removing '{date_dir}' directory in caches: {str(e)}")
+                print_error(f"Error removing '{file_dir}' directory in caches: {str(e)}")

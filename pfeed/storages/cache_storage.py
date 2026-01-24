22f7ef155a98aa8af62908cd50eddf38d8cb6a2b
softwareentrepreneer <softwareentrepreneer@gmail.com>
2026-01-24 10:47:41 -0500

    fix: unpredictable iteration order in CacheStorage

--- pfeed/storages/cache_storage.py
+++ pfeed/storages/cache_storage.py
@@ -1,45 +1,55 @@
-import shutil
-import datetime
+from typing import Literal
+
 from pathlib import Path
 
 from pfeed.enums import DataLayer
 from pfeed.storages.local_storage import LocalStorage
+from pfeed.config import get_config
+
+
+config = get_config()
 
 
 class CacheStorage(LocalStorage):
-    # FIXME: move to init?
-    NUM_RETAINED_DAYS = 7
+    DEFAULT_NUM_RETAINED_DAYS = 7
 
     def __init__(
         self,
-        data_layer: DataLayer,
+        data_layer: DataLayer=DataLayer.CLEANED,
+        data_domain: str | Literal['MARKET_DATA', 'NEWS_DATA'] = 'MARKET_DATA',
         data_path: Path | None = None,
-        storage_options: dict | None=None,
+        storage_options: dict | None = None,
+        num_retained_days: int = DEFAULT_NUM_RETAINED_DAYS,
     ):
         super().__init__(
             data_layer=data_layer,
+            data_domain=data_domain,
             data_path=data_path,
             storage_options=storage_options,
         )
+        self.num_retained_days = num_retained_days
         self._clear_caches()
     
     def _clear_caches(self):
         '''Clear old caches except the current date'''
-        from pfund import print_error
+        import shutil
+        from datetime import timedelta
         from pfund_kit.utils import get_last_modified_time
+        from pfund_kit.style import cprint, TextStyle, RichColor
         from pfund_kit.utils.temporal import get_today
-        from pfeed.config import get_config
-        config = get_config()
+
         cache_path = config.cache_path
         today = get_today()
         
-        for file_dir in Path(cache_path).rglob("*"):
+        if not cache_path.exists():
+            return
+        for file_dir in cache_path.iterdir():
             if not file_dir.is_dir():
                 continue
             last_modified_date = get_last_modified_time(file_dir).date()
             try:
-                if last_modified_date < today - datetime.timedelta(days=self.NUM_RETAINED_DAYS):
+                if last_modified_date < today - timedelta(days=self.num_retained_days):
                     shutil.rmtree(file_dir)
-                    print(f"Removed '{file_dir}' directory in caches")
+                    cprint(f"Removed '{file_dir}' directory in caches", style=TextStyle.BOLD + RichColor.YELLOW)
             except Exception as e:
-                print_error(f"Error removing '{file_dir}' directory in caches: {str(e)}")
+                cprint(f"Error removing '{file_dir}' directory in caches: {str(e)}", style=TextStyle.BOLD + RichColor.RED)

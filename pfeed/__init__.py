1af66a20f3d033703502cc650ca94d3221dd4eed
softwareentrepreneer <softwareentrepreneer@gmail.com>
2026-05-17 02:48:45 -0400

    remove resample_data in init

--- pfeed/__init__.py
+++ pfeed/__init__.py
@@ -15,7 +15,6 @@ if TYPE_CHECKING:
     from pfeed.engine import DataEngine
     from pfeed.sources.pfund import PFund
     from pfeed.sources.alphafund import AlphaFund
-    from pfeed._etl.market import resample_data
     # from pfeed.sources.financial_modeling_prep import (
     #     FinancialModelingPrep,
     #     FinancialModelingPrep as FMP,
@@ -38,9 +37,6 @@ def __getattr__(name: str):
     elif name == 'alias':
         from pfeed.utils.aliases import ALIASES
         return ALIASES
-    elif name == 'resample_data':
-        from pfeed._etl.market import resample_data
-        return resample_data
     elif name == 'plot':
         import pfund_plot as plot
         return plot
@@ -89,8 +85,6 @@ __all__ = (
     "YahooFinance", "YFinance", "YF",
     "Bybit",
     # "FinancialModelingPrep", "FMP",
-    # utils
-    "resample_data",
 )
 def __dir__():
     return sorted(__all__)

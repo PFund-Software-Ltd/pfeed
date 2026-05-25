214d77988985d26e8d7673238c7fb4face562860
softwareentrepreneer <softwareentrepreneer@gmail.com>
2026-05-25 05:48:50 -0400

    docs: add README.py stub

---
+++ afterpython/README.py
@@ -0,0 +1,24 @@
+# ruff: noqa
+
+import marimo
+
+__generated_with = "0.23.8"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import pfeed as pe
+
+    return
+
+
+@app.cell
+def _():
+    print("testing")
+    return
+
+
+if __name__ == "__main__":
+    app.run()
